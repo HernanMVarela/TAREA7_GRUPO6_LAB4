eb62c29cdd20dbca99d39b8c2cb86a3c5e992b52
Hest10 <hermest10@aol.com>
2022-06-01 19:15:43 -0300

    Seguro.java
    This reverts commit 6ecae0cca914ec62cc587ed58a46685f4dc5dd34.

--- src/entidad/Seguro.java
+++ src/entidad/Seguro.java
@@ -1,5 +1,103 @@
 package entidad;
 
 public class Seguro {
+	private int id;
+	static int cont=1;
+	private String descripcion;
+	private TipoSeguro tipo;
+	private String costo;
+	private String costoMaximo;
+	
+	public Seguro() {
+		
+	}
+	
+	public Seguro(String descripcion,String costo,String costoMaximo){
+		super();
+		id = devuelveProximoId();
+		this.descripcion =descripcion;
+		this.costo = costo;
+		this.costoMaximo = costo;				
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+
+	public TipoSeguro getTipo() {
+		return tipo;
+	}
+
+
+
+
+	public void setTipo(TipoSeguro tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+
+	public String getCosto() {
+		return costo;
+	}
+
+
+
+
+	public void setCosto(String costo) {
+		this.costo = costo;
+	}
+
+
+
+
+	public String getCostoMaximo() {
+		return costoMaximo;
+	}
+
+
+
+
+	public void setCostoMaximo(String costoMaximo) {
+		this.costoMaximo = costoMaximo;
+	}
+
+
+
+
+	public static int devuelveProximoId() {
+		return cont++;
+
+		}
+	
+	public int getTipoid() {
+		//return tipo.getTipoId();
+		return 0;
+	}
+	
+	public String getTipoDesc() {
+		//return tipo.getDescripcion_Tipo();
+		return "";
+	}
+	
 
 }
